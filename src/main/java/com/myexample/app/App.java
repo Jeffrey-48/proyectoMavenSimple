17da838dba9f594aedfa9e346540d0914ce1505b
Jeffrey portatil <javargasg_1@uqvirtual.edu.co>
2021-12-30 15:05:56 -0500

    proyecto maven simple

---
+++ src/main/java/com/myexample/app/App.java
@@ -0,0 +1,13 @@
+package com.myexample.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
